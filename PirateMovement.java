640ef4a59d7d2f09a1e0f128954053fbdf5ac6c7
Octavian <35154776+MrSilverX@users.noreply.github.com>
2018-03-06 17:37:08 -0600

    Add files via upload

---
+++ PirateMovement.java
@@ -0,0 +1,8 @@
+import java.awt.Point;
+
+public interface PirateMovement {
+	void goNorth(Point cur);
+	void goEast(Point cur);
+	void goWest(Point cur);
+	void goSouth(Point cur);
+}
---
+++ doublePirateShip.java
@@ -0,0 +1,30 @@
+import java.awt.Point;
+
+public class doublePirateShip implements PirateMovement{
+
+	@Override
+	public void goNorth(Point cur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void goEast(Point cur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void goWest(Point cur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void goSouth(Point cur) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
